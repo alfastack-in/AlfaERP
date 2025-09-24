045570b362b1a104a82c610da837c972fe3478bf
rawsun007 <roshanramani.dev@gmail.com>
2025-09-24 18:31:59 +0530

    doctype add: Beta Tester Contact

---
+++ alfaerp/alfaerp/doctype/beta_tester_contact/test_beta_tester_contact.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, roshan ramani and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestBetaTesterContact(FrappeTestCase):
+	pass
