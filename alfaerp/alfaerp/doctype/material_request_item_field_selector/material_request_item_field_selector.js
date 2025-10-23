44e1e19d1ed7c5c2d73b8944edca6d60f5223c22
Alvish Ubuntu <alvishramani.dev@gmail.com>
2025-10-23 12:58:01 +0000

    add: create Material Request Field Selector and Material Request Item Field Selector doctype files

---
+++ alfaerp/alfaerp/doctype/material_request_item_field_selector/material_request_item_field_selector.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, roshan ramani and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Material Request Item Field Selector", {
+// 	refresh(frm) {
+
+// 	},
+// });
