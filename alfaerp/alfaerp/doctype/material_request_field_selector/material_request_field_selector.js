77d3f8b2bff2879b196aadca24d24659154175a9
Alvish Ubuntu <alvishramani.dev@gmail.com>
2025-11-04 10:13:09 +0000

    refactor: remove Material Request Field Selector, Material Request Item Field Selector and Quality Inspection Field Selector

--- alfaerp/alfaerp/doctype/material_request_field_selector/material_request_field_selector.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, roshan ramani and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Material Request Field Selector", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- alfaerp/alfaerp/doctype/quality_inspection_field_selector/quality_inspection_field_selector.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, roshan ramani and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Quality Inspection Field Selector", {
-// 	refresh(frm) {
-
-// 	},
-// });
