1a5b9e81fc1fff4b792f4bf8d22110879a1f5470
Alvish Ramani <80829856+Alvish0407@users.noreply.github.com>
2026-04-13 12:15:19 +0530

    refactor: remove beta tester contact and help & support doctype files (#3)

--- alfaerp/alfaerp/doctype/beta_tester_contact/beta_tester_contact.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, roshan ramani and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Beta Tester Contact", {
-// 	refresh(frm) {
-
-// 	},
-// });
