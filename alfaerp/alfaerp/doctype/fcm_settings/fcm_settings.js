1cdd19013737433038465c56a519102f52e4935a
Aman Boora <aman.chaudharyboora@gmail.com>
2026-04-13 15:45:09 +0530

    Fixed, FCM Setting to be harcoded in the site config, Updated user permission for user device details

--- alfaerp/alfaerp/doctype/fcm_settings/fcm_settings.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, roshan ramani and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("FCM Settings", {
-// 	refresh(frm) {
-
-// 	},
-// });
